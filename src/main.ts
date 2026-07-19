5dce28cbc234d82b9397af2cb733400e76f13e7c
hub-feedback <hi@ben.gy>
2026-07-19 10:46:45 +1000

    Add feedback widget (footer trigger -> feedback.benrichardson.dev)

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,8 @@
+// feedback:begin (managed by hub/scripts/feedback/backfill.mjs)
+import { mountFeedback } from './feedback';
+mountFeedback();
+// feedback:end
+
 import { init } from './app.ts';
 
 init().catch((err: unknown) => {
